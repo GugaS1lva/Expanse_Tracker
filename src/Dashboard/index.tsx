a277da8c52d6ac1c06e0202067852449795f1374
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-08-23 10:53:30 -0300

    Update index.tsx

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -11,6 +11,11 @@ const Dashboard = () => {
         { id: '3', title: 'Spotfy', category: 'music', cost: 19.90 },
         { id: '4', title: 'YTMusic', category: 'music', cost: 16.90 },
         { id: '5', title: 'LOL', category: 'game', cost: 0.00 },
+        { id: '6', title: 'Netflix', category: 'streaming', cost: 39.90 },
+        { id: '7', title: 'Amazon', category: 'streaming', cost: 9.99 },
+        { id: '8', title: 'Spotfy', category: 'music', cost: 19.90 },
+        { id: '9', title: 'YTMusic', category: 'music', cost: 16.90 },
+        { id: '10', title: 'LOL', category: 'game', cost: 0.00 },
     ]
 
     const handleNavigateToAddNewExpanse = () => {
@@ -30,7 +35,7 @@ const Dashboard = () => {
                 </nav>
             </header>
 
-            <main className="w-full bg-purple-400 max-w-screen-lg h-5/6 grid overflow-y-scroll content-start grid-cols-3 gap-4 p-4 mb-10">
+            <main className="w-full bg-[#2F2E41] max-w-screen-lg h-5/6 grid overflow-y-scroll content-start grid-cols-3 gap-4 p-4 mb-10 rounded-lg">
                 {cards?.map((item) => (
                     <Card
                         key={item.id}
